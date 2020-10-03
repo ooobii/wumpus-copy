0d8511ef8a8bae2cc1b18dd5b25735660ab12eb7
Matthew Wendel <matthewwendel77@gmail.com>
2020-10-03 15:14:45 -0400

    add main script

---
+++ wumpus-copy.php
@@ -0,0 +1,33 @@
+<?php
+#load packages
+include __DIR__.'/vendor/autoload.php';
+
+#load helper methods
+require __DIR__.'/lib/config_manager.php'; //config_manager
+
+
+// START =================================
+// load config from json
+$config = new config_manager('config.json');
+
+// Prepare historical message arrays
+$srcHistories = array();
+$destHistories = array();
+
+// Connect to Discord API
+$discord = new \Discord\Discord([
+    'token' => $config->get_token(),
+    'logging' => FALSE
+]);
+
+$discord->on('ready', function($discord) use ($config) {
+    
+});
+
+$discord->run();
+
+
+
+
+
+
