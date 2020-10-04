91897f538cb4ecf94fb7a8559cd7e3f5661eb283
Matthew Wendel <matt_wendel@hotmail.com>
2020-10-03 22:28:58 -0400

    pass message handling from Discord to monitor.

--- wumpus-copy.php
+++ wumpus-copy.php
@@ -2,16 +2,16 @@
 #load packages
 include __DIR__ . '/vendor/autoload.php';
 
-#load helper methods
-require __DIR__ . '/lib/config_manager.php'; //config_manager
+#load classes
+require __DIR__ . '/lib/say.php';
+require __DIR__ . '/lib/ChannelConnection.php';
+require __DIR__ . '/lib/ChannelMonitor.php';
+require __DIR__ . '/lib/DestinationChannel.php';
+require __DIR__ . '/lib/config_manager.php'; 
 
 // START =================================
-// load config from json
+// load config from JSON and 
 $config = new config_manager('config.json');
-
-// Prepare historical message arrays
-$srcHistories = array();
-$destHistories = array();
 
 // Connect to Discord API
 $discord = new \Discord\Discord([
@@ -20,6 +20,12 @@ $discord = new \Discord\Discord([
 ]);
 
 $discord->on('ready', function ($discord) use ($config) {
+
+    $discord->on('message', function ($message) use ($config) {
+        foreach($config->get_monitors() as $monitor) {
+            $monitor->process_message($config, $message);
+        }
+    });
 
 });
 
