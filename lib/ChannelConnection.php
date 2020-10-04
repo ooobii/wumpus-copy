6cdf87d15f02eb3a62c33f95c202fcf2ef4ff220
Matthew Wendel <matthewwendel77@gmail.com>
2020-10-04 15:20:43 -0400

    update channel connectin base class

--- lib/ChannelConnection.php
+++ lib/ChannelConnection.php
@@ -2,15 +2,27 @@
 
 class ChannelConnection
 {
-    public function __construct($isSource, $channelId)
+    public function __construct($isSource, $guildId, $channelId, $nickname)
     {
-        if (!$channelId) throw new Exception("A channel ID is required when creating a channel details model.");
-        $this->ChannelID = $channelId;
+        if (!$guildId || empty($guildId)) throw new Exception("A guild ID is required when creating a channel details model.");
+        $this->GuildId = $guildId;
+
+        if (!$channelId || empty($channelId)) throw new Exception("A channel ID is required when creating a channel details model.");
+        $this->ChannelId = $channelId;
+
+        if (!$nickname || empty($nickname)) throw new Exception("A connection nickname is required when creating a channel details model.");
+        $this->Nickname = $nickname;
     }
 
 
     public function get_channel_id() {
-        return $this->ChannelID;
+        return $this->ChannelId;
+    }
+    public function get_guild_id() {
+        return $this->GuildId;
+    }
+    public function get_nickname() {
+        return $this->Nickname;
     }
 
 }
