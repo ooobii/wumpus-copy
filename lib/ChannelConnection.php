5def6fe48ee0ac7aace3f13aace88724af6f2834
Matthew Wendel <matt_wendel@hotmail.com>
2020-10-03 21:19:16 -0400

    rename 'chan' to 'ChannelConnection'

---
+++ lib/ChannelConnection.php
@@ -0,0 +1,21 @@
+<?php
+
+class ChannelConnection
+{
+    public function __construct($nickname, $isSource, $guildId, $channelId)
+    {
+        if ($guildId) {
+            $this->GuildId = $guildId;
+        }
+
+        if (!$channelId) {
+            throw new Exception("A channel ID is required when creating a channel details model.");
+        }
+
+        $this->ChannelID = $channelId;
+
+        #TODO: Finish Chan class construction
+    }
+
+    #TODO: Add additional fetching capabilities to properties of Chan class.
+}
