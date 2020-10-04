f98e3d622250d7e1e7ea5fd51e1c1e9bdf667e33
Matthew Wendel <matt_wendel@hotmail.com>
2020-10-03 22:11:50 -0400

    require guildid and add fetches to ChannelConnection base

--- lib/ChannelConnection.php
+++ lib/ChannelConnection.php
@@ -4,19 +4,19 @@ class ChannelConnection
 {
     public function __construct($isSource, $guildId, $channelId)
     {
+        if (!$guildId) throw new Exception("A guild ID is required when creating a channel details model.");
+        $this->GuildId = $guildId;
 
-        if ($guildId) {
-            $this->GuildId = $guildId;
-        }
-
-        if (!$channelId) {
-            throw new Exception("A channel ID is required when creating a channel details model.");
-        }
-
+        if (!$channelId) throw new Exception("A channel ID is required when creating a channel details model.");
         $this->ChannelID = $channelId;
-
-        #TODO: Finish Chan class construction
     }
 
-    #TODO: Add additional fetching capabilities to properties of Chan class.
+
+    public function get_guildid() {
+        return $this->GuildId;
+    }
+    public function get_channelid() {
+        return $this->ChannelId;
+    }
+
 }
