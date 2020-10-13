39cf130876cab619046c68fae5a1752c0111e4b2
Matthew Wendel <matthewwendel77@gmail.com>
2020-10-12 22:35:44 -0400

    configtests: add config manager test class

---
+++ test/configuration_manager/tests_config_manager.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require __DIR__ . '/../../lib/ChannelConnection.php';
+require __DIR__ . '/../../lib/ChannelMonitor.php';
+require __DIR__ . '/../../lib/DestinationChannel.php';
+require __DIR__ . '/../../lib/config_manager.php'; 
+
+
+class tests_config_manager extends TestCase
+{
+
+}
