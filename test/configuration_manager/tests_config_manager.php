fe60a60a80b0bea2ea2f7f79a3a788896d525d9a
Matthew Wendel <matthewwendel77@gmail.com>
2020-10-12 22:36:48 -0400

    configtests: simple - add parse test make sure the config class loads property values into the proper class fields.

--- test/configuration_manager/tests_config_manager.php
+++ test/configuration_manager/tests_config_manager.php
@@ -10,4 +10,34 @@ require __DIR__ . '/../../lib/config_manager.php';
 class tests_config_manager extends TestCase
 {
 
+    public function testParsing()
+    {
+        #define test variant type
+        $fileVariant = 'valid_simple';
+
+
+        #load the valid test configuration.
+        $testConfig = new config_manager("test/configuration_manager/json/testConfig_$fileVariant.json");
+
+        
+        #make sure config is not empty
+        $this->assertNotEmpty($testConfig);
+
+
+        #make sure both source monitors have been loaded
+        $this->assertCount(
+            2, 
+            $testConfig->get_monitors(), 
+            "Configuration manager didn't load the correct number of source monitors (File: testConfig_$fileVariant.json)."
+        );
+
+
+        #make sure only two destinations were created
+        $this->assertCount(
+            2,
+            $testConfig->get_deposits(),
+            "Configuration manager didn't load the correct number of deposit destinations (File: testConfig_$fileVariant.json)."
+        );
+
+    }
 }
